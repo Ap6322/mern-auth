4b219798c9223c67aeeff77f6b5be278c637f61f
Aakash Pandey <pandeyaakash6322@gmail.com>
2026-02-08 03:35:32 +0530

    Add Verify OTP controller and also add cypto for otp Hashing

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,6 +3,7 @@ import { userModel } from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { transporter } from "../config/nodemailer.js";
+import crypto from "crypto";
 
 export const register = async (req, res) => {
   try {
@@ -142,6 +143,63 @@ export const logout = async (req, res) => {
     });
     console.log("Logged out successfully");
     return res.json({ success: true, message: "Logged Out Successfully" });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+// Send dverification OTP to the user's Email
+
+export const sendVerifyOtp = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const user = await userModel.findById(userId);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+    if (user.isAccountVerified) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Account Already Verified." });
+    }
+    const otp = String(Math.floor(100000 + Math.random() * 900000));
+    // Hash OTP before saving
+    const hashedOtp = crypto.createHash("sha256").update(otp).digest("hex");
+
+    user.verifyOtp = hashedOtp;
+    user.verifyOtpExpireAt = Date.now() + 10 * 60 * 1000;
+
+    await user.save();
+
+    const mailOptions = {
+      from: process.env.SENDER_EMAIL, // must be verified in Brevo
+      to: user.email,
+      subject: `Account Verification OTP`,
+      html: `
+    <h2>Welcome to Aakash Tech</h2>
+    <p>Your OTP is ${otp}. Verify your account using this OTP.</p>
+    <p>This OTP is valid for 10 Minutes only.</p>
+    <p>Do not share with anyone</p>
+    <p><b>Email:</b> ${user.email}</p>
+    <br/>
+    <p>Thanks for joining us!</p>
+  `,
+    };
+    try {
+      const info = await transporter.sendMail(mailOptions);
+      console.log("Verification OTP sent on email:", info.messageId);
+    } catch (mailErr) {
+      console.error("Email error:", mailErr.message);
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Verification OTP sent to email",
+    });
   } catch (error) {
     return res.status(500).json({
       success: false,
